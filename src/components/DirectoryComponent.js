fb775a8ef4bff3aef878b7fadbfc970ba087c8f0
Sachin Rajbhandari <sachink.rb@gmail.com>
2021-03-10 14:28:00 -0600

    Adding to the Single Page App

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-function RenderDirectoryItem({ campsite}) {
+function RenderDirectoryItem({ campsite }) {
     return (
         <Card>
             <CardImg width="100%" src={campsite.image} alt={campsite.name} />
