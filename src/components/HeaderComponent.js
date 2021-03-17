ca6df3eb8353fb3928245cd7ca3c466e978f17ab
Sachin Rajbhandari <sachink.rb@gmail.com>
2021-03-17 15:55:29 -0500

    Intro to Redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import {
     Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron,
     Button, Modal, ModalHeader, ModalBody,
-    Form, FormGroup, Input, Label} from 'reactstrap';
+    Form, FormGroup, Input, Label
+} from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
