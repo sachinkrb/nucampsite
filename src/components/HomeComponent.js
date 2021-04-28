e7d2e438a95d5171e3c5df3a413c90042b525c30
Sachin Rajbhandari <sachink.rb@gmail.com>
2021-04-28 13:20:02 -0500

    Updated baseUrl to work with netlify

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -45,7 +45,10 @@ function Home(props) {
                         errMess={props.promotionErrMess} />
                 </div>
                 <div className="col-md m-1">
-                    <RenderCard item={props.partner} />
+                    <RenderCard
+                        item={props.partner}
+                        isLoading={props.partnersLoading}
+                        errMess={props.partnersErrMess} />
                 </div>
             </div>
         </div>
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -40,6 +40,44 @@ export const addCampsites = campsites => ({
     payload: campsites
 });
 
+export const fetchPartners = () => dispatch => {
+    dispatch(partnersLoading());
+
+    return fetch(baseUrl + 'partners')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                error.response = response;
+                throw error;
+            }
+        },
+            error => {
+                const errMess = new Error(error.message);
+                throw errMess;
+            }
+        )
+        .then(response => response.json())
+        .then(partners => dispatch(addPartners(partners)))
+        .catch(error => dispatch(partnersFailed(error.message)));
+};
+
+export const partnersLoading = () => ({
+    type: ActionTypes.PARTNERS_LOADING
+});
+
+export const partnersFailed = errMess => ({
+    type: ActionTypes.PARTNERS_FAILED,
+    payload: errMess
+});
+
+export const addPartners = partners => ({
+    type: ActionTypes.ADD_PARTNERS,
+    payload: partners
+});
+
+
 export const fetchComments = () => dispatch => {
     return fetch(baseUrl + 'comments')
         .then(response => {
@@ -76,7 +114,7 @@ export const addComment = comment => ({
     payload: comment
 });
 export const postComment = (campsiteId, rating, author, text) => dispatch => {
-    
+
     const newComment = {
         campsiteId: campsiteId,
         rating: rating,
@@ -92,7 +130,7 @@ export const postComment = (campsiteId, rating, author, text) => dispatch => {
             "Content-Type": "application/json"
         }
     })
-    .then(response => {
+        .then(response => {
             if (response.ok) {
                 return response;
             } else {
@@ -101,14 +139,44 @@ export const postComment = (campsiteId, rating, author, text) => dispatch => {
                 throw error;
             }
         },
-        error => { throw error; }
-    )
-    .then(response => response.json())
-    .then(response => dispatch(addComment(response)))
-    .catch(error => {
-        console.log('post comment', error.message);
-        alert('Your comment could not be posted\nError: ' + error.message);
-    });
+            error => { throw error; }
+        )
+        .then(response => response.json())
+        .then(response => dispatch(addComment(response)))
+        .catch(error => {
+            console.log('post comment', error.message);
+            alert('Your comment could not be posted\nError: ' + error.message);
+        });
+}
+
+export const postFeedback = (feedback) => () => {
+
+    return fetch(baseUrl + 'feedback', {
+        method: "POST",
+        body: JSON.stringify(feedback),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                error.response = response;
+                throw error;
+            }
+        },
+            error => { throw error; }
+        )
+        .then(response => response.json())
+        .then(response => {
+            alert("Thank you for your feedback!")
+        })
+        .catch(error => {
+            console.log('post feedback', error.message);
+            alert('Your feedback could not be posted\nError: ' + error.message);
+        });
 }
 
 export const fetchPromotions = () => dispatch => {
